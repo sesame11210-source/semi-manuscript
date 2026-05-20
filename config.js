574b7bade6d40b848b5fd24ecd4b7297bbab02a7
sesame11210-source <sesame11210@gmail.com>
2026-05-20 11:12:19 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// SEMI 프로그램 설정 파일
+// 이 파일은 본인 PC에만 보관하세요. GitHub에 올리지 마세요.
+const SEMI_CONFIG = {
+  apiKey: "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"
+};
